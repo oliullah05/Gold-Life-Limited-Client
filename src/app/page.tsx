ca5a6b6e6ead3be826eae236f63ae0d0a7570536
shahadat52 <shahadathossain.sh255@gmail.com>
2024-10-11 21:32:48 +0600

    feat: career page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,13 @@ export default function Home() {
 
 
 
-return (
-<div className="h-screen w-screen flex justify-center items-center">
-<Button className="bg-green-500 text-3xl py-12 px-7" variant="destructive">Shadcn running</Button>
-</div>
+    return (
+        <div className="h-screen w-screen flex justify-center items-center">
+            <Button className="bg-green-500 text-3xl py-12 px-7" variant="destructive">Shadcn running</Button>
+            <Button className="bg-green-500 text-3xl py-12 px-7" variant="destructive">Shadcn running</Button>
 
-)
+        </div>
+
+    )
 
 }
