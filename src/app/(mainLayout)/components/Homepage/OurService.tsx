1789fb16e03fb959abcae5515eaac58568be8b2a
JAHID HASAN <developerjahid05@gmail.com>
2024-10-27 17:17:48 +0600

    styles: service card resposive

--- src/app/(mainLayout)/components/Homepage/OurService.tsx
+++ src/app/(mainLayout)/components/Homepage/OurService.tsx
@@ -1,59 +1,61 @@
-import React from 'react';
-import ServiceCard from '../shared/ServiceCard/ServiceCard';
-
-
-
+import React from "react";
+import ServiceCard from "../shared/ServiceCard/ServiceCard";
 const cardData = [
-    {
-      title: 'Implants',
-      description: 'Nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia',
-      imageSrc: "https://medito-react.vercel.app/static/media/service-2-1.964b5d28b409e4d88b30.jpg",
-      linkText: 'Learn More',
-    },
-    {
-      title: 'Braces',
-      description: 'Nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia',
-      imageSrc: "https://medito-react.vercel.app/static/media/service-2-1.964b5d28b409e4d88b30.jpg",
-      linkText: 'Learn More',
-    },
-    {
-      title: 'Teeth Whitening',
-      description: 'Nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia',
-      imageSrc: "https://medito-react.vercel.app/static/media/service-2-1.964b5d28b409e4d88b30.jpg",
-      linkText: 'Learn More',
-    },
-    {
-      title: 'Teeth Removal',
-      description: 'Nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia',
-      imageSrc: "https://medito-react.vercel.app/static/media/service-2-1.964b5d28b409e4d88b30.jpg",
-      linkText: 'Learn More',
-    },
-    {
-      title: 'Tooth Checkup',
-      description: 'Nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia',
-      imageSrc: "https://medito-react.vercel.app/static/media/service-2-1.964b5d28b409e4d88b30.jpg",
-      linkText: 'Learn More',
-    },
-    {
-      title: 'Straighten Teeth',
-      description: 'Nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia',
-      imageSrc: "https://medito-react.vercel.app/static/media/service-2-1.964b5d28b409e4d88b30.jpg",
-      linkText: 'Learn More',
-    },
-  ];
+  {
+    title: "Implants",
+    description:
+      "Nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia",
+    imageSrc:
+      "https://medito-react.vercel.app/static/media/service-2-1.964b5d28b409e4d88b30.jpg",
+  },
+  {
+    title: "Braces",
+    description:
+      "Nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia",
+    imageSrc:
+      "https://medito-react.vercel.app/static/media/service-2-1.964b5d28b409e4d88b30.jpg",
+  },
+  {
+    title: "Teeth Whitening",
+    description:
+      "Nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia",
+    imageSrc:
+      "https://medito-react.vercel.app/static/media/service-2-1.964b5d28b409e4d88b30.jpg",
+  },
+  {
+    title: "Teeth Removal",
+    description:
+      "Nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia",
+    imageSrc:
+      "https://medito-react.vercel.app/static/media/service-2-1.964b5d28b409e4d88b30.jpg",
+  },
+  {
+    title: "Tooth Checkup",
+    description:
+      "Nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia",
+    imageSrc:
+      "https://medito-react.vercel.app/static/media/service-2-1.964b5d28b409e4d88b30.jpg",
+  },
+  {
+    title: "Straighten Teeth",
+    description:
+      "Nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia",
+    imageSrc:
+      "https://medito-react.vercel.app/static/media/service-2-1.964b5d28b409e4d88b30.jpg",
+  },
+];
 
-export default function Home() {
+export default function OurService() {
   return (
-    <div className="primary-container bg-red-500 mx-auto  grid gap-4 sm:grid-cols-2 lg:grid-cols-3 justify-between">
-    {cardData.map((data, index) => (
-      <ServiceCard
-        key={index}
-        title={data.title}
-        description={data.description}
-        imageSrc={data.imageSrc}
-        linkText={data.linkText}
-      />
-    ))}
-  </div>
+    <div className="primary-container  place-items-center border justify-items-center mx-auto  grid gap-4 sm:grid-cols-2 lg:grid-cols-3 ">
+      {cardData.map((data, index) => (
+        <ServiceCard
+          key={index}
+          title={data.title}
+          description={data.description}
+          imageSrc={data.imageSrc}
+        />
+      ))}
+    </div>
   );
 }
--- src/app/(mainLayout)/components/shared/ServiceCard/ServiceCard.tsx
+++ src/app/(mainLayout)/components/shared/ServiceCard/ServiceCard.tsx
@@ -10,22 +10,22 @@ type CardProps = {
 
 const ServiceCard: React.FC<CardProps> = ({ title, description, imageSrc,  }) => {
   return (
-    <div className="w-92 mx-auto bg-white rounded-lg shadow-lg overflow-hidden transition-all duration-300 hover:shadow-xl">
+    <div className=" w-full max-w-[22rem] mx-auto bg-white rounded-lg shadow-lg overflow-hidden transition-all duration-300 hover:shadow-xl">
       <div className="relative h-64 w-full overflow-hidden">
         <Image
-          src={imageSrc}
+          src={imageSrc} 
           alt={title}
           layout="fill"
           objectFit="cover"
-          className="transition-transform duration-500 ease-in-out transform hover:scale-105"
-        />
+          className="transition-transform duration-1000 ease-in-out transform hover:scale-110"
+        /> 
       </div>
-      <div className="p-4">
+      <div className="p-4 py-8">
         <h3 className="text-lg font-semibold mb-2">{title}</h3>
         <p className="text-gray-600 text-sm mb-4">
-          {description || 'No description available.'}
+          {description }
         </p>
-        <a href="#" className="text-blue-500 hover:underline flex items-center gap-1">
+        <a href="#" className="text-primary_color text-underLine-animation flex items-center gap-1">
           Learn More <span aria-hidden="true">→</span>
         </a>
       </div>
