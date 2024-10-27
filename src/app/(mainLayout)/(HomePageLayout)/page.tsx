9f6593d2a53b29cc74d6abc9742f5b2b900447af
JAHID HASAN <developerjahid05@gmail.com>
2024-10-27 16:17:54 +0600

    Feat: newsletter section added

--- src/app/(mainLayout)/(HomePageLayout)/page.tsx
+++ src/app/(mainLayout)/(HomePageLayout)/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroSection from '../components/Homepage/HeroSection';
 import AboutUs from '../components/Homepage/AboutUs';
+import NewsletterForm from '../components/shared/NewsLetter/NewsLetter';
 
 const page = () => {
     return (
         <div>
             <HeroSection/>
             <AboutUs/>
+            <NewsletterForm/>
         </div> 
     );
 };
